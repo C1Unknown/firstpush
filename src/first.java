6fd67823ca378d5905e0290235ead8a7e6ab5437
Mike Fischer <mike@matshark.de>
2021-10-03 15:16:40 +0200

    update1

--- src/first.java
+++ src/first.java
@@ -2,5 +2,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("First");
         System.out.println("Second");
+        System.out.println("Third");
     }
 }
